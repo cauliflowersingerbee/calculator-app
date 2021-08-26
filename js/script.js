3921afb705f39540e383735300d52fa06e33f5f5
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-08-26 21:47:00 +0200

    wrote list of programming tasks

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* flow of events
+1. set up event listeners for following: 
+
+- number key (0–9)
+- operator key (+, -, ×, ÷)
+- decimal key
+- equals key
+- clear key
+
+2. 
+
+*/
+//number key
+
+const 
